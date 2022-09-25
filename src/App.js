36ebf167f4b84fe596f7d09a9e794c56deaaaaff
Sumaia Akter <sumaiaakter@Sumaias-MBP.lan>
2022-09-25 12:04:16 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Todos from './components/Todos';
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>redux with react</h1>
+      <Todos/>
     </div>
   );
 }
 
 export default App;
+
+// step1: install packages
+// step2: constant define
+// step3: async action creator
+// step4: reducer
+// step5: create store
+// step6: provide store from index.js file
+// step7: use store using useSelector
+// step8: adding css style 
